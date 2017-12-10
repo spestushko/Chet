8b96f911c7def79d8171ef0eff54264d3b14de13
Sergei <spestushko@gmail.com>
2017-12-10 18:12:37 -0500

    Fixed socket issue. Add chat func WIP. TBD Add eth int

--- mean-app/routes/chat.js
+++ mean-app/routes/chat.js
@@ -8,36 +8,54 @@ var Chat = require('../models/Chat.js');
 
 server.listen(4000);
 
-/**
-* Socket.io
-*/
+// socket io
 io.on('connection', function (socket) {
-  console.log(`User connected`);
+  console.log('User connected');
   socket.on('disconnect', function() {
-    console.log(`User disconnected`);
+    console.log('User disconnected');
   });
-  socket.on('save-message', function(data) {
-    console.log(`Save message data: ${data}`);
-    io.emit('new-message', { message:data });
+  socket.on('save-message', function (data) {
+    console.log(data);
+    io.emit('new-message', { message: data });
   });
 });
 
-/**
-* Get all chats
-*/
+/* GET ALL CHATS */
 router.get('/:room', function(req, res, next) {
-  Chat.find({ room: req.params.room }, function(err, chats) {
+  Chat.find({ room: req.params.room }, function (err, chats) {
     if (err) return next(err);
     res.json(chats);
   });
 });
 
-/**
-* Save chat
-*/
+/* GET SINGLE CHAT BY ID */
+router.get('/:id', function(req, res, next) {
+  Chat.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* SAVE CHAT */
 router.post('/', function(req, res, next) {
-  Chat.create(req.body, function(err, post) {
-    if (err) return next (err);
+  Chat.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* UPDATE CHAT */
+router.put('/:id', function(req, res, next) {
+  Chat.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* DELETE CHAT */
+router.delete('/:id', function(req, res, next) {
+  Chat.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
     res.json(post);
   });
 });
